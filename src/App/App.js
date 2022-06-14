a15431d5c2c742afc00a1e90f27bfe45ebe42b1d
Leonardo Arantes <leonardorarantes95@gmail.com>
2022-06-14 20:47:22 -0300

    feat: add login page

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import SignUp from "../SignUp/Index.js";
+import Login from "../Login/index.js";
 
 function App(){
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
       </Routes>
     </BrowserRouter>
