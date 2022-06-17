480e328cb6b00c65248c5c325a4da785992272ce
Rafael <rafaelongo45@gmail.com>
2022-06-17 15:54:18 -0300

    Feat: After seatching a username in the search bar, user can click anywhere in the page to hide the results

--- src/App/App.js
+++ src/App/App.js
@@ -5,13 +5,17 @@ import Login from "../Login/index.js";
 import SignUp from "../SignUp/Index.js";
 import Timeline from "../Timeline/TimelinePage.js";
 import UserContext from "../Contexts/UserContext.js";
+import ClickContext from "../Contexts/HeaderClickContext.js";
 
 function App(){
+  const [click, setClick] = useState(false);
   const [userInfo, setUserInfo] = useState({name: '', profileImage: '', userId: ''});
+
   
   return (
     <BrowserRouter>
       <UserContext.Provider value={{userInfo, setUserInfo}}>
+      <ClickContext.Provider value={{click, setClick}}>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
@@ -19,6 +23,7 @@ function App(){
           <Route path='/timeline' element={<Timeline filter={"timeline"} />} />
           <Route path='/hashtag/:hashtag' element={<Timeline filter={"hashtag"} />} />
         </Routes>
+      </ClickContext.Provider>
       </UserContext.Provider>
     </BrowserRouter>
   )
